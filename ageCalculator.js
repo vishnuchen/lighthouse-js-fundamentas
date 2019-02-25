a93a0cdfa9dfabb3ee6ee682c9cfd7d57f3945ca
Vishnu Chennuru <vishnuchen@gmail.com>
2019-02-25 02:16:30 +0000

    first 2 coding task done

---
+++ ageCalculator.js
@@ -0,0 +1,6 @@
+function ageCalculator(name, yearofBirth, currentYear) {
+  return name + ' is ' + (currentYear - yearofBirth) +  ' years old.';
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
---
+++ howmany100.js
@@ -0,0 +1,8 @@
+function howManyHundreds(num) {
+  return Math.trunc(num / 100);
+}
+console.log(howManyHundreds(1000));
+console.log(howManyHundreds(894));
+console.log(howManyHundreds(520));
+console.log(howManyHundreds(99));
+console.log(howManyHundreds(0));
